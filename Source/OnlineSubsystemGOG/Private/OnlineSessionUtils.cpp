3046ee3daff2d625321ad775cf1a7bf3889c3ce9
Aleksej Pawlowskij <apawlowskij@gog.com>
2019-02-24 15:55:58 +0100

    Fill lobby ping when retrieving lobbies

--- Source/OnlineSubsystemGOG/Private/OnlineSessionUtils.cpp
+++ Source/OnlineSubsystemGOG/Private/OnlineSessionUtils.cpp
@@ -176,7 +176,15 @@ bool OnlineSessionUtils::Fill(const FUniqueNetIdGOG& InLobbyID, FOnlineSession& 
 
 bool OnlineSessionUtils::Fill(const FUniqueNetIdGOG& InLobbyID, FOnlineSessionSearchResult& InOutOnlineSessionSearchResult)
 {
-	InOutOnlineSessionSearchResult.PingInMs = 0;
+	auto ping = galaxy::api::Networking()->GetPingWith(InLobbyID);
+	auto err = galaxy::api::GetError();
+	if (err)
+	{
+		UE_LOG_ONLINE(Warning, TEXT("Failed to get ping with lobby: lobbyID='%s'; %s: %s"),
+			*InLobbyID.ToString(), UTF8_TO_TCHAR(err->GetName()), UTF8_TO_TCHAR(err->GetMsg()));
+	}
+
+	InOutOnlineSessionSearchResult.PingInMs = ping > 0 ? ping : MAX_QUERY_PING;
 
 	return Fill(InLobbyID, InOutOnlineSessionSearchResult.Session) && InOutOnlineSessionSearchResult.IsValid();
 }
